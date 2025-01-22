8e6b444832e5e1cddd217a15538fd165c5088ab0
Remo Senekowitsch <remo@buenzli.dev>
2025-01-22 23:48:03 +0100

    Add snapshot testing setup

--- src/lib.rs
+++ src/lib.rs
@@ -302,3 +302,40 @@ pub fn main() -> eyre::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn snapshot() {
+    main().unwrap();
+    let timestamped_files = ["releases.json", "feed.xml"];
+    let inexplicably_non_deterministic_files = ["images/2023-08-rust-survey-2022/experiences.png"];
+    insta::glob!("..", "site/**/*", |path| {
+        if path.is_dir() {
+            return;
+        }
+        let path = path.display().to_string();
+        if timestamped_files
+            .into_iter()
+            .chain(inexplicably_non_deterministic_files)
+            .any(|f| path.ends_with(f))
+        {
+            // Skip troublesome files, e.g. they might contain timestamps.
+            // If possible, they are tested separately below.
+            return;
+        }
+        let content = fs::read(path).unwrap();
+        // insta can't deal with non-utf8 strings?
+        let content = String::from_utf8_lossy(&content).into_owned();
+        insta::assert_snapshot!(content);
+    });
+
+    // test files with timestamps filtered
+    insta::with_settings!({filters => vec![
+        (r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}", "(filtered timestamp)"),
+    ]}, {
+        for file in timestamped_files {
+            let content = fs::read(format!("site/{file}")).unwrap();
+            let content = String::from_utf8_lossy(&content).into_owned();
+            insta::assert_snapshot!(content);
+        }
+    });
+}
